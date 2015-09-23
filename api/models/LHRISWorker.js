9c46d799873f7df90e7bf3bdecde0b5156c10e10
Joshua Chan <joshua@appdevdesigns.net>
2015-09-23 03:26:23 -0700

    + Added model class methods for finding worker and information and assignment regions

--- api/models/LHRISWorker.js
+++ api/models/LHRISWorker.js
@@ -5,6 +5,8 @@
 * @docs        :: http://sailsjs.org/#!documentation/models
 */
 
+var AD = require('ad-utils');
+
 module.exports = {
 
     // tableName:"lhris_account",
@@ -42,6 +44,149 @@ module.exports = {
         }
 
 
+    },
+    
+    ////////////////////////////
+    // Model class methods
+    ////////////////////////////
+    
+    /**
+     * Used by workersByAccount().
+     *
+     * [
+     *     {
+     *          "Name": <string>,
+     *          "Account Number": <string>,
+     *          "Phone": <string>,
+     *          "Email": <string>,
+     *          "location_id": <int>
+     *     },
+     *     ...
+     * ]
+     *
+     * @param string countryCode
+     * @return Deferred
+     */
+    findWorkersInfo: function(countryCode) {
+        var dfd = AD.sal.Deferred();
+        countryCode = countryCode.replace(/\W/g, '') // alphanumeric only
+                        || '%'; // default is all countries
+        
+        LHRISWorker.query("\
+            SELECT \
+                CONCAT(r.ren_surname, ', ', r.ren_givenname) AS 'Name', \
+                a.account_number AS 'Account Number', \
+                GROUP_CONCAT( p.phone_number, ' (', SUBSTRING(pt.phonetype_label, 1, 1), ')' ) AS 'Phone', \
+                e.email_address AS 'Email', \
+                lt.location_id \
+            \
+            FROM \
+                hris_country_data cd \
+                JOIN hris_account a \
+                    ON cd.country_code LIKE '" + countryCode + "' \
+                    AND a.country_id = cd.country_id \
+                \
+                JOIN hris_worker w \
+                    ON w.account_id = a.account_id \
+                JOIN hris_ren_data r \
+                    ON w.ren_id = r.ren_id \
+                \
+                LEFT JOIN hris_phone_data p \
+                    ON p.ren_id = r.ren_id \
+                LEFT JOIN hris_phonetype_trans pt \
+                    ON p.phonetype_id = pt.phonetype_id \
+                    AND pt.language_code = 'en' \
+                LEFT JOIN hris_email e \
+                    ON r.ren_id = e.ren_id \
+                    AND e.email_issecure = 1 \
+                \
+                LEFT JOIN hris_assignment asgn \
+                    ON r.ren_id = asgn.ren_id \
+                    AND asgn.assignment_isprimary = 1 \
+                LEFT JOIN hris_assign_team_data asgn_t \
+                    ON asgn.team_id = asgn_t.team_id \
+                LEFT JOIN hris_xref_team_location xtl \
+                    ON asgn.team_id = xtl.team_id \
+                LEFT JOIN hris_assign_location_trans lt \
+                    ON xtl.location_id = lt.location_id \
+                    AND lt.language_code = 'en' \
+            \
+            GROUP BY \
+                r.ren_id \
+        ", function(err, results) {
+            if (err) {
+                dfd.reject(err);
+            } else {
+                dfd.resolve(results);
+            }
+        });
+        
+        return dfd;
+    },
+    
+    
+    /**
+     * Find workers from a given country and list them by their account numbers.
+     *
+     * Each worker's region, secure email and telephone numbers are included.
+     * Region is derived from the worker's primary assignment.
+     *
+     * Telephone numbers have (H), (O), or (M) appended depending on the type
+     * of number (home, office, mobile). If a worker has multiple phone numbers
+     * they will all be included, separated by commas.
+     *
+     * @param object options
+     *          - string countryCode
+     *                Optional. Default is to find staff from all countries.
+     *          - boolean groupByRegion
+     *                Default is false.
+     * @return Deferred
+     */
+    workersByAccount: function(options) {
+        var dfd = AD.sal.Deferred();
+        
+        options = options || {};
+        options.countryCode = options.countryCode || '';
+        options.groupByRegion = options.groupByRegion || false;
+        
+        LHRISWorker.findWorkersInfo(options.countryCode)
+        .fail(dfd.reject)
+        .done(function(workers) {
+            
+            LHRISAssignLocation.mapToRegion()
+            .fail(dfd.reject)
+            .done(function(locations, regions) {
+                // Use the location+region lists to add to the worker info
+                var workersByRegion = {};
+                var workersByAccount = {};
+                for (var i=0; i<workers.length; i++) {
+                    var locationID = workers[i].location_id;
+                    var accountNum = parseInt(workers[i]['Account Number'].replace(/\D/, ''));
+                    if (locations[locationID]) {
+                        var regionID = locations[locationID].region_location_id;
+                        var regionLabel = regions[regionID].name;
+                    } else {
+                        var regionLabel = 'none';
+                    }
+                    workers[i].Region = regionLabel;
+                    
+                    // Grouped by region
+                    workersByRegion[regionLabel] = workersByRegion[regionLabel] || {};
+                    workersByRegion[regionLabel][accountNum] = workers[i];
+                    
+                    // Flat list
+                    workersByAccount[accountNum] = workers[i];
+                }
+                
+                if (options.groupByRegion) {
+                    dfd.resolve(workersByRegion);
+                } else {
+                    dfd.resolve(workersByAccount);
+                }
+            });
+        });
+        
+        return dfd;
     }
 };
 
