3141c0fb313de31dfe60be5b1f5b816311bee562
Joshua Chan <joshua@appdevdesigns.net>
2015-12-03 01:47:04 -0800

    * Fixed character encoding issue

--- api/models/LNSSRen.js
+++ api/models/LNSSRen.js
@@ -757,83 +757,102 @@ module.exports = {
             throw new Error('legacy_hris connection not defined in the config');
         }
         
-        // If region was specified, then filter results by adding a HAVING 
-        // clause to the sql.
-        var regionCode = regionCode || null;
-        var havingClause = "";
-        if (typeof regionCode == 'string') {
-            havingClause = " HAVING region = ? ";
-        }
+        var staff = [];
         
-        LNSSRen.query(" \
-            SELECT \
-                CONCAT( \
-                    r.ren_surname, ', ', r.ren_givenname, \
-                    ' (', r.ren_preferredname, ')' \
-                ) AS name, \
-                r.ren_namecharacters AS chineseName, \
-                REPLACE(a.account_number, '-', '') AS accountNum, \
-                nr.nssren_salaryAmount AS baseSalary, \
-                nr.nssren_ytdBalance AS accountBal, \
-                e.email_address AS email, \
-                GROUP_CONCAT(DISTINCT \
-                    p.phone_number, ' (', \
-                    SUBSTRING(ptt.phonetype_label, 1, 1), ')' \
-                    SEPARATOR ', ' \
-                ) AS phone, \
-                SUBSTRING( \
-                    t.territory_desc, 1, LOCATE('-', t.territory_desc)-1 \
-                ) AS region, \
-                GROUP_CONCAT(t.territory_desc SEPARATOR ', ') AS territory, \
-                r.ren_isfamilypoc AS poc, \
-                xtl.location_id, \
-                nr.ren_guid, \
-                nr.nssren_id \
-            \
-            FROM \
-                "+hris+".hris_ren_data AS r \
-                JOIN nss_core_ren AS nr \
-                    ON nr.ren_guid = r.ren_guid \
-                    AND nr.nssren_isActive = 1 \
-                \
-                JOIN "+hris+".hris_worker AS w \
-                    ON r.ren_id = w.ren_id \
-                JOIN "+hris+".hris_account AS a \
-                    ON w.account_id = a.account_id \
-                \
-                JOIN nss_core_renterritory AS rt \
-                    ON nr.nssren_id = rt.nssren_id \
-                JOIN nss_core_territory AS t \
-                    ON rt.territory_id = t.territory_id \
-                \
-                LEFT JOIN "+hris+".hris_phone_data AS p \
-                    ON r.ren_id = p.ren_id \
-                LEFT JOIN "+hris+".hris_phonetype_trans AS ptt \
-                    ON p.phonetype_id = ptt.phonetype_id \
-                    AND ptt.language_code = 'en' \
-                LEFT JOIN "+hris+".hris_email AS e \
-                    ON r.ren_id = e.ren_id \
-                    AND e.email_issecure = 1 \
-                \
-                LEFT JOIN "+hris+".hris_assignment AS asgn \
-                    ON r.ren_id = asgn.ren_id \
-                    AND asgn.assignment_isprimary = 1 \
-                LEFT JOIN "+hris+".hris_assign_team_data AS asgn_t \
-                    ON asgn.team_id = asgn_t.team_id \
-                LEFT JOIN "+hris+".hris_xref_team_location AS xtl \
-                    ON asgn.team_id = xtl.team_id \
-            \
-            GROUP BY \
-                r.ren_id \
-            \
-            "+ havingClause +" \
-            \
-        ", [regionCode], function(err, staff) {
-            if (err) {
-                dfd.reject(err);
-            } else {
+        async.series([
+            function(next) {
+                // HRIS uses utf8 encoding
+                LNSSRen.query("SET NAMES utf8", function(err) {
+                    if (err) next(err);
+                    else next();
+                });
+            },
+            
+            function(next) {
+                // If region was specified, then filter results by adding a
+                // HAVING clause to the sql.
+                var regionCode = regionCode || null;
+                var havingClause = "";
+                if (typeof regionCode == 'string') {
+                    havingClause = " HAVING region = ? ";
+                }
+                
+                LNSSRen.query(" \
+                    SELECT \
+                        CONCAT( \
+                            r.ren_surname, ', ', r.ren_givenname, \
+                            ' (', r.ren_preferredname, ')' \
+                        ) AS name, \
+                        r.ren_namecharacters AS chineseName, \
+                        REPLACE(a.account_number, '-', '') AS accountNum, \
+                        nr.nssren_salaryAmount AS baseSalary, \
+                        nr.nssren_ytdBalance AS accountBal, \
+                        e.email_address AS email, \
+                        GROUP_CONCAT(DISTINCT \
+                            p.phone_number, ' (', \
+                            SUBSTRING(ptt.phonetype_label, 1, 1), ')' \
+                            SEPARATOR ', ' \
+                        ) AS phone, \
+                        SUBSTRING( \
+                            t.territory_desc, 1, LOCATE('-', t.territory_desc)-1 \
+                        ) AS region, \
+                        GROUP_CONCAT(t.territory_desc SEPARATOR ', ') AS territory, \
+                        r.ren_isfamilypoc AS isPOC, \
+                        xtl.location_id, \
+                        nr.ren_guid, \
+                        nr.nssren_id \
+                    \
+                    FROM \
+                        "+hris+".hris_ren_data AS r \
+                        JOIN nss_core_ren AS nr \
+                            ON nr.ren_guid = r.ren_guid \
+                            AND nr.nssren_isActive = 1 \
+                        \
+                        JOIN "+hris+".hris_worker AS w \
+                            ON r.ren_id = w.ren_id \
+                        JOIN "+hris+".hris_account AS a \
+                            ON w.account_id = a.account_id \
+                        \
+                        JOIN nss_core_renterritory AS rt \
+                            ON nr.nssren_id = rt.nssren_id \
+                        JOIN nss_core_territory AS t \
+                            ON rt.territory_id = t.territory_id \
+                        \
+                        LEFT JOIN "+hris+".hris_phone_data AS p \
+                            ON r.ren_id = p.ren_id \
+                        LEFT JOIN "+hris+".hris_phonetype_trans AS ptt \
+                            ON p.phonetype_id = ptt.phonetype_id \
+                            AND ptt.language_code = 'en' \
+                        LEFT JOIN "+hris+".hris_email AS e \
+                            ON r.ren_id = e.ren_id \
+                            AND e.email_issecure = 1 \
+                        \
+                        LEFT JOIN "+hris+".hris_assignment AS asgn \
+                            ON r.ren_id = asgn.ren_id \
+                            AND asgn.assignment_isprimary = 1 \
+                        LEFT JOIN "+hris+".hris_assign_team_data AS asgn_t \
+                            ON asgn.team_id = asgn_t.team_id \
+                        LEFT JOIN "+hris+".hris_xref_team_location AS xtl \
+                            ON asgn.team_id = xtl.team_id \
+                    \
+                    GROUP BY \
+                        r.ren_id \
+                    \
+                    "+ havingClause +" \
+                    \
+                ", [regionCode], function(err, results) {
+                    if (err) {
+                        next(err);
+                    } else {
+                        staff = results;
+                        next();
+                    }
+                });
+            },
+            
+            function(next) {
                 LHRISAssignLocation.mapToRegion()
-                .fail(dfd.reject)
+                .fail(next)
                 .done(function(locations, regions) {
                     // Merge HRIS team region into the staff entries
                     for (var i=0; i<staff.length; i++) {
@@ -846,9 +865,13 @@ module.exports = {
                         }
                         staff[i].regionHRIS = regionLabel;
                     }
-                    dfd.resolve(staff);
+                    next();
                 });
             }
+            
+        ], function(err) {
+            if (err) dfd.reject(err);
+            else dfd.resolve(staff);
         });
         
         return dfd;
