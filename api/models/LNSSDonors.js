a6dcc46def552125b34b390e45dc36886b38995e
Joshua Chan <joshua@appdevdesigns.net>
2019-03-07 01:50:44 -0800

    getDonorTotals with optional date filters

--- api/models/LNSSDonors.js
+++ api/models/LNSSDonors.js
@@ -5,6 +5,7 @@
 * @docs        :: http://sailsjs.org/#!documentation/models
 */
 
+var moment = require('moment');
 var Entities = require('html-entities').AllHtmlEntities;
 var entities = new Entities();
 
@@ -24,7 +25,8 @@ module.exports = {
 
     attributes: {
 
-        donor_id : {
+        'id' : {
+            columnName: 'donor_id',
             type : "integer",
             size : 11,
             primaryKey : true,
@@ -134,7 +136,62 @@ module.exports = {
         0: 'Normal',
         99: 'Anonymous local',
         98: 'Anonymous other'
-    }
+    },
+    
+    
+    /**
+     * @param {string|Date} [startDate]
+     * @param {string|Date} [enbDate]
+     * @return {Promise}
+     */
+    getDonorTotals(startDate=null, endDate=null) {
+        return new Promise((resolve, reject) => {
+            // Optional date conditions
+            var params = [];
+            var dateCondition = '';
+            if (startDate) {
+                params.push(moment(startDate).format('YYYY-MM-DD'));
+                dateCondition += ` AND batch.donBatch_dateProcessed >= ? `;
+            }
+            if (endDate) {
+                params.push(moment(endDate).format('YYYY-MM-DD'));
+                dateCondition += ` AND batch.donBatch_dateProcessed <= ? `;
+            }
+            
+            LNSSDonors.query(`
+                SELECT
+                    donor.*,
+                    SUM(item.donItem_amount) AS totalAmount,
+                    COUNT(item.donItem_id) AS numGifts,
+                    COUNT(DISTINCT batch.nssren_id) AS numStaff,
+                    COUNT(DISTINCT batch.nsc_territory_id) AS numLocations
+                
+                FROM
+                    nss_don_donors AS donor
+                    JOIN nss_don_donItem AS item
+                        ON donor.donor_id = item.donor_id
+                    JOIN nss_don_donBatch AS batch
+                        ON item.donBatch_id = batch.donBatch_id
+                
+                WHERE
+                    -- Only count donations that were received
+                    batch.donBatch_status = "Received"
+                    -- Exclude anonymous donors
+                    AND donor.donors_type < 98
+                    
+                    ${dateCondition}
+                    
+                GROUP BY
+                    donor.donor_id
+            
+            `, params, (err, list) => {
+                if (err) reject(err);
+                else {
+                    resolve(list);
+                }
+            });
+        });
+    },
     
     
 };
